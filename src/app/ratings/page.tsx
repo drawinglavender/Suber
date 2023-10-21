383f294c0da5ed38ecb5a549cc23010c97764087
KaiHikichi <kai.hikichi@gmail.com>
2023-10-21 13:26:25 -0700

    hi

---
+++ src/app/ratings/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { Slider } from "@/components/ui/slider"
+
+
+
+const Page = () => {
+  return (
+    <div>
+      <h2 className={`mb-3 text-2xl font-semibold`}>
+            User Ratings{' '}
+        
+          </h2>
+
+        <p>
+            How would you like to rate your driver/passenger "driver name"
+            </p>    
+
+      <Slider defaultValue={[2.5]} max={5} step={1} />
+
+      <Button variant="outline">Submit</Button>
+
+
+    </div>
+
+  )
+}
+
+export default Page
