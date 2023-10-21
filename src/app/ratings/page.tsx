56c294f6210b5f34b0edf1d3f490407c050f87da
KaiHikichi <kai.hikichi@gmail.com>
2023-10-21 14:09:09 -0700

    hi

--- src/app/ratings/page.tsx
+++ src/app/ratings/page.tsx
@@ -1,24 +1,27 @@
 'use client'
 import { Button } from "@/components/ui/button"
 import { Slider } from "@/components/ui/slider"
-
+import {useState} from 'react'
 
 
 const Page = () => {
+    const [sliderValue, setSliderValue] = useState(2.5)
   return (
     <div>
       <h2 className={`mb-3 text-2xl font-semibold`}>
             User Ratings{' '}
-        
+            Value = {sliderValue}
           </h2>
 
         <p>
-            How would you like to rate your driver/passenger "driver name"
+            How would you like to rate your driver/passenger "name"
             </p>    
 
-      <Slider defaultValue={[2.5]} max={5} step={1} />
-
-      <Button variant="outline">Submit</Button>
+      <Slider defaultValue={[sliderValue]} max={5} step={0.5} onValueCommit={(value)=> {setSliderValue(value)}}/>
+        
+      <Button variant="outline" onClick={() => {
+        console.log(sliderValue)
+      }}>Submit</Button>
 
 
     </div>
