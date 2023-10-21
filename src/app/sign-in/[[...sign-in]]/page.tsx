80bdef5d5fd70a4c599240165aab9d7029006410
teeverc <teeverc@gmail.com>
2023-10-21 13:14:31 -0700

    pretty + ingore package lock

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,8 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    
-<div className='flex justify-center'>
-      <SignIn/>
-</div>
-    
+    <div className='flex justify-center'>
+      <SignIn />
+    </div>
   )
 }
