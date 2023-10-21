e647861b3454f7239d286af876390cbc1934ecec
KaiHikichi <kai.hikichi@gmail.com>
2023-10-21 12:53:57 -0700

    shadcn init

---
+++ src/app/create/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+
+const Page = () => {
+  return (
+    <div>
+      <h1>create</h1>
+      <p className={''}>
+            Find in-depth information about Next.js features and API.
+        </p>
+    </div>
+
+  )
+}
+
+export default Page
