4dd05d7e6c1a0adc8d2e9befb2404b154828c764
teeverc <teeverc@gmail.com>
2023-10-21 16:24:49 -0700

    home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,25 +3,30 @@
 import { useUser } from '@clerk/nextjs'
 import FeedCard from '@/components/FeedCard'
 import Listing from '@/types/listing'
-
-const listings: Listing[] = [
-  {
-    id: 1,
-    created_at: 'created_at',
-    userID: 'userID',
-    userName: 'Joe M',
-    userImage: 'userImage',
-    location: 'Burnaby',
-    seatsAvailable: 1,
-    leavingTime: 123,
-  }
-]
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Page = () => {
-  const { user, isLoaded } = useUser()
-
-  if (!isLoaded) return <div>Loading...</div>
+  const [listings, setListings] = useState<Listing[]>([])
   
+  useEffect(() => {
+    const getAndSetListings = async () => {
+
+      const resp = await fetch('/api/listings')
+
+      const listings = await resp.json()
+
+      if (resp.ok) {
+        setListings(listings)
+      }
+    }
+    getAndSetListings()
+  })
+
+  if (listings.length === 0) {
+    return <div className=''>No listings</div>
+  }
+
   return <div className=''>{listings.map((listing) => <FeedCard key={listing.id} listing={listing} />)}</div>
 }
 
