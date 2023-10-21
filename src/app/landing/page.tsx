a6420aa6e2976f64f7e82135007fc0ec42c58499
teeverc <teeverc@gmail.com>
2023-10-21 14:42:04 -0700

    finish CreateForm

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function landing() {
-  return (
-    <div>welcum page</div>
-  )
+  return <div>welcum page</div>
 }
--- src/components/CreateForm.tsx
+++ src/components/CreateForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
@@ -10,6 +12,7 @@ import {
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import {
   Select,
   SelectContent,
@@ -20,11 +23,40 @@ import {
   SelectValue
 } from '@/components/ui/select'
 
+const neighborhoods = [
+  'Arbutus Ridge',
+  'Downtown',
+  'Dunbar-Southlands',
+  'Fairview',
+  'Grandview-Woodland',
+  'Hastings-Sunrise',
+  'Kensington-Cedar Cottage',
+  'Kerrisdale',
+  'Killarney',
+  'Kitsilano',
+  'Marpole',
+  'Mount Pleasant',
+  'Oakridge',
+  'Renfrew-Collingwood',
+  'Riley Park',
+  'Shaughnessy',
+  'South Cambie',
+  'Strathcona',
+  'Sunset',
+  'Victoria-Fraserview',
+  'West End',
+  'West Point Grey'
+]
+
 export default function CreateForm() {
+  const [leaveTime, setLeaveTime] = useState('10')
+  const [seats, setSeats] = useState(1)
+  const [location, setLocation] = useState('')
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant='outline'>Edit Profile</Button>
+        <Button variant='outline'>Create Listing</Button>
       </DialogTrigger>
       <DialogContent className='sm:max-w-[425px]'>
         <DialogHeader>
@@ -36,11 +68,27 @@ export default function CreateForm() {
             <Label htmlFor='location' className='text-right'>
               Location
             </Label>
-            <Input
-              id='location'
-              defaultValue='Location'
-              className='col-span-3'
-            />
+            <Select
+              value={location}
+              onValueChange={(value) => setLocation(value)}
+            >
+              <SelectTrigger className='col-span-3'>
+                <SelectValue placeholder='Select a location' />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Location</SelectLabel>
+                  <ScrollArea className='h-72 rounded-md'>
+                    {neighborhoods.map((neighborhood) => (
+                      <SelectItem key={neighborhood} value={neighborhood}>
+                        {neighborhood}
+                      </SelectItem>
+                    ))}
+                  </ScrollArea>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
           <div className='grid grid-cols-4 items-center gap-4'>
             <Label htmlFor='seats' className='text-right'>
@@ -49,23 +97,33 @@ export default function CreateForm() {
             <Input
               id='seats'
               type='number'
-              defaultValue={1}
+              value={seats}
+              min={1}
+              max={99}
+              onChange={(e) => setSeats(parseInt(e.target.value))}
               className='col-span-3'
             />
           </div>
           <div className='grid grid-cols-4 items-center gap-4'>
-          <Label htmlFor='seats' className='text-right'>
-            Leave Time
+            <Label htmlFor='seats' className='text-right'>
+              Leaving in:
             </Label>
-            <Select>
+            <Select
+              value={leaveTime}
+              onValueChange={(value) => setLeaveTime(value)}
+            >
               <SelectTrigger className='col-span-3'>
                 <SelectValue placeholder='Select a time' />
               </SelectTrigger>
-              
+
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>Time</SelectLabel>
-                  <SelectItem value='apple'>Apple</SelectItem>
+                  {[10, 15, 20, 25, 30].map((time) => (
+                    <SelectItem key={time} value={time.toString()}>
+                      {time + ' minutes'}{' '}
+                    </SelectItem>
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
