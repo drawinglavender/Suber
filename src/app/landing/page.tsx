5d96fa358fd3bc845ae858dff4e8f3c523c433c5
Han Pham <giahan0427@gmail.com>
2023-10-21 13:20:14 -0700

    hi

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { Button } from "@/components/ui/button"
+
+
+const Page = () => {
+  return (
+    <div>
+      <h1>create</h1>
+      <Button>Button</Button>
+
+      <p className={''}>
+            Find in-depth information about Next.js features and API.
+        </p>
+    </div>
+
+  )
+}
+
+export default Page
