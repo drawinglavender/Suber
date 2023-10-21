6776ed9794aa58b29cee8a69f0a13d73ab910cfe
KaiHikichi <kai.hikichi@gmail.com>
2023-10-21 12:56:24 -0700

    btn

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,10 +1,13 @@
 'use client'
+import { Button } from "@/components/ui/button"
 
 
 const Page = () => {
   return (
     <div>
       <h1>create</h1>
+      <Button>Button</Button>
+
       <p className={''}>
             Find in-depth information about Next.js features and API.
         </p>
