bbcf68e7f2528c7bd91f6864fdef3ed0766b50ca
teeverc <teeverc@gmail.com>
2023-10-21 11:24:18 -0700

    add supabase

---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+if (!supabaseUrl) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
+
+const supabasePublicKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabasePublicKey) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
+
+const supabase = createClient(supabaseUrl, supabasePublicKey)
