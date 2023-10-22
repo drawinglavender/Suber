685b539ec0fd3e8b9f1475acd2b13efceb75c4a7
teeverc <teeverc@gmail.com>
2023-10-21 19:07:37 -0700

    add readme, move /home to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,58 @@
-import Image from 'next/image'
-import { SignOutButton } from '@clerk/nextjs'
+'use client'
 
-export default function Home() {
+import { useEffect, useState } from 'react'
+import { useUser } from '@clerk/nextjs'
+
+import db from '@/lib/dbAnon'
+import FeedCard from '@/components/FeedCard'
+import Listing from '@/types/listing'
+
+const Page = () => {
+  const [listings, setListings] = useState<Listing[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    const getAndSetListings = async () => {
+      const listings = await db
+        .from('listings')
+        .select('*')
+        .gt('leaveTime', new Date().getTime())
+
+        setLoading(false)
+      if (listings.error) {
+        return
+      }
+
+      setListings(listings.data.reverse())
+    }
+
+    getAndSetListings()
+
+    db.channel('listings').on(
+      'postgres_changes',
+      { event: 'INSERT', schema: 'public', table: 'listings' },
+      (payload) => {
+        // @ts-ignore
+        setListings((listings) => [payload.new, ...listings])
+      }
+    )
+  })
+
+  if (listings.length === 0 && !loading) {
+    return (<div className='w-full flex justify-center font-bold text-2xl'>No listings available...</div>)
+  }
+
+  if (loading) {
+    return (<div className='w-full flex justify-center font-bold text-2xl'>Loading...</div>)
+  }
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <div className=''>
+      {listings.map((listing) => (
+        <FeedCard key={listing.id} listing={listing} />
+      ))}
+    </div>
   )
 }
+
+export default Page
